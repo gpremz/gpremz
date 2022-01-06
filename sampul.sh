3f8851441eb157b80ef6ca7d72e127745a949621
premg <gpremz@gmail.com>
2022-01-06 11:35:22 +0530

    echo updated   angin

--- sampul.sh
+++ sampul.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo install nginx
+echo
 yum install nginx -y
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
@@ -11,7 +11,10 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-master static README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl enable nginx
 systemctl restart nginx
+
+
 
 
 
