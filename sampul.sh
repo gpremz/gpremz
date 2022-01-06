ef5558428aa049957879dd10acd0ad8b3b53c3aa
premg <gpremz@gmail.com>
2022-01-06 11:12:17 +0530

    echo updated

--- sampul.sh
+++ sampul.sh
@@ -1,15 +1,18 @@
-#! /bin/bash
+#!/bin/bash
 
-echo ${yum install nginx -y}
-echo ${systemctl enable nginx}
-echo ${systemctl start nginx}
-echo ${curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"}
 
-echo ${cd /usr/share/nginx/html}
-echo ${rm -rf *}
-echo ${unzip /tmp/frontend.zip}
-echo ${mv frontend-main/* .}
-echo ${mv static/* .}
-echo ${rm -rf frontend-master static README.md}
-echo ${mv localhost.conf /etc/nginx/default.d/roboshop.conf}
-echo ${systemctl restart nginx}
+ echo
+ yum install nginx -y
+ systemctl enable nginx
+ systemctl start nginx
+ curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+ cd /usr/share/nginx/html
+ rm -rf *
+ unzip /tmp/frontend.zip
+ mv frontend-main/* .
+ mv static/* .
+ rm -rf frontend-master static README.md
+ mv localhost.conf /etc/nginx/default.d/roboshop.conf
+ systemctl restart nginx
+
